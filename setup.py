4fe5c95e2613d841d472eb2fc3de3f4d01bb5b46
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2021-09-08 11:27:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup  # type: ignore[import]
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='karelcraft',
     url='https://github.com/melvincabatuan/KarelCraft',
     author='Melvin Cabatuan',
     author_email='melvincabatuan@gmail.com',
-    packages=['karelcraft'],
+#     packages=['karelcraft'],
+    packages=find_packages(include=('karelcraft*',)),
     install_requires=['ursina'],
     include_package_data=True,
     python_requires='>=3.8',
